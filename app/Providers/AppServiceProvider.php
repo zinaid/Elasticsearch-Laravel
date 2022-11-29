010060b76026ddb6bcc08864ba3a2121a5c90b11
Zinaid Kapić <zinaid_kapic@hotmail.com>
2022-11-29 23:30:56 +0100

    Binding search repository and eloquent search to service providers

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Papers\PapersRepository;
+use App\Papers\EloquentSearch;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +15,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->bind(SearchRepository::class, EloquentSearch::class);
     }
 
     /**
